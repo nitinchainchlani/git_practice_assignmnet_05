37dddaa4cf9162bc3316ccf074e4697ca70a4ac1
nitin chainchlani <nitin.chainchlani534@gmail.com>
2022-12-06 20:38:42 +0530

    Prime code pushed by nitin

---
+++ prime.js
@@ -0,0 +1,22 @@
+function checkPrime(num){
+let counter=0;
+for(let i=1; i<=num; i++){
+if(num%i==1){
+counter++;
+
+}
+}
+if(counter==2){
+return true;
+}
+return false;
+
+}
+
+let ans= checkPrime(13);
+
+if(ans==true) {
+console.log("prime")
+}else{
+console.log("not prime")
+}
